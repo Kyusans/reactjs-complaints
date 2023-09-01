92f438dd4de9e71477cf91243ea539130c841796
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-09-01 14:11:44 +0800

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import { Card, Form } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Card, Form, FloatingLabel, Container, Button} from 'react-bootstrap'
+import "./css/site.css";
 
 export default function Login() {
+	const [userId, setUserId] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
 		<>
-			<Card>
-				<Card.Head><h2>Login</h2></Card.Head>
-			</Card>
+			<Container fluid="md" className='centered'>
+				<Card className="card-thin" border='success'>
+					<Card.Header><h2>Login</h2></Card.Header>
+					<Card.Body>
+						<Form>
+							<Form.Group className='mt-3 mb-3 fatter-text centered-label'>
+								<FloatingLabel label="Id">
+									<Form.Control type='text' value={userId} onChange={(e) => setUserId(e.target.value)} placeholder='Id' autoFocus required/>
+								</FloatingLabel>
+							</Form.Group>
+							<Form.Group className='mb-4 fatter-text centered-label'>
+								<FloatingLabel label="Password">
+									<Form.Control type='text' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' required/>
+								</FloatingLabel>
+							</Form.Group>
+
+							<Container className='text-center'>
+								<Button className='button-large btn-lg' variant='outline-success'>Login</Button>
+							</Container>
+						</Form>
+					</Card.Body>
+				</Card>
+			</Container>
 		</>
 	)
 }
