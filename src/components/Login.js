05454613a9565ebb65e7a406cdecae405ee32429
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-09-01 13:49:07 +0800

    added axios and site.css

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card, Form } from 'react-bootstrap'
+
+export default function Login() {
+	return (
+		<>
+			<Card>
+				<Card.Head><h2>Login</h2></Card.Head>
+			</Card>
+		</>
+	)
+}
