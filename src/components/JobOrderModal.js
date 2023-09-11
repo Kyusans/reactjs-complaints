0c711ee2838773e89d946fb5426fc82ccb0281e7
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-09-11 12:04:03 +0800

    Create JobOrderModal.js

---
+++ src/components/JobOrderModal.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Col, Form, Modal, Row } from 'react-bootstrap'
+
+function JobOrderModal(props) {
+  const {show, onHide} = props;
+
+  return (
+    <div>
+      <Modal show={show} onHide={onHide}>
+        <Modal.Header closeButton><h3>Job order creation</h3></Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group as={Row}> 
+              <Form.Label column sm="2">Ticket #</Form.Label>
+              <Col sm="10">
+                <Form.Control plaintext readOnly defaultValue="#69" />
+              </Col>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  )
+}
+
+export default JobOrderModal
