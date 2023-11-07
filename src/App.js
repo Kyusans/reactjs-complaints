04d1d7a61d325952a7522620060bb4281a9daad8
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-11-07 12:19:42 +0800

    now redirects from http to https

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './components/Login';
 import MainLayoutRoutes from './MainLayoutRoutes';
+import { useEffect } from 'react';
 
 function App() {
   if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/gsd/service-worker.js')
     .then((registration) =>{
       console.log("Service Worker registration with scope: " + registration.scope);
     })
@@ -13,6 +14,11 @@ function App() {
       console.error("Service Worker registration failed: " + err);
     })
   }
+  useEffect(() =>{
+    if (window.location.protocol === 'http:') {
+      window.location.href = `https://${window.location.host}${window.location.pathname}`;
+    }
+  },[])
   return (
     <>
       <Router basename='/gsd'>
