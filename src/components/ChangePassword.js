328d06d9f3a80174d0d8472100d089fb28bd4aad
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-11-16 12:32:58 +0800

    added getCurrentPassword in change password script

--- src/components/ChangePassword.js
+++ src/components/ChangePassword.js
@@ -1,18 +1,58 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import "./css/site.css";
 import { Button, Card, Container, FloatingLabel, Form } from 'react-bootstrap';
+import axios from 'axios';
 
 function ChangePassword() {
+  const [currentPass, setCurrentPass] = useState("");
   const [currentPassword, setCurrentPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
+  // const [isCurrentPassword, setIsCurrentPassword] = useState(true);
   const [validated, setValidated] = useState(false);
+  const [showInvalidCurrent, setShowInvalidCurrent] = useState(false);
+
+  const getCurrentPassword = useCallback(async () => {
+    try {
+      const userId = localStorage.getItem("facultyLoggedIn") === "true" ? localStorage.getItem("facCode") : localStorage.getItem("userId");
+      const url = localStorage.getItem("url") + "users.php";
+      const jsonData = { userId: userId };
+      const formData = new FormData();
+      formData.append("operation", "getCurrentPassword");
+      formData.append("json", JSON.stringify(jsonData));
+      const response = await axios({url: url, data: formData, method: 'post'});
+      if (response.data !== 0) {
+        setShowInvalidCurrent(false);
+        setCurrentPass(localStorage.getItem("facultyLoggedIn") === "true" ? response.data.fac_password : response.data.user_password);
+      }
+    } catch (error) {
+      alert("There was an error: " + error);
+    }
+  },[]);
+  
+
   const handleSubmit = (e) => {
     const form = e.currentTarget;
-
-    if(form.checkValidity()) {
-
+    getCurrentPassword();
+    if(currentPass !== currentPassword){
+      setCurrentPassword("");
+      setTimeout(() => {setShowInvalidCurrent(true);}, 1000);
+      e.preventDefault();
+      e.stopPropagation();
+    }else if(form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }else if(form.checkValidity() === true) {
+      e.preventDefault();
+      e.stopPropagation();
+      alert("Success!");
     }
+
+    setValidated(true);
   }
+  useEffect(() => {
+    getCurrentPassword();
+  },[getCurrentPassword])
+  
 
   return (
     <Container className='centered'>
@@ -23,6 +63,7 @@ function ChangePassword() {
             <FloatingLabel label="Current Password">
               <Form.Control type='password' value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} placeholder='Current Password' autoFocus required/>
             </FloatingLabel>
+            {showInvalidCurrent && <Form.Text className='text-danger'>Invalid current password</Form.Text>}
           </Form.Group>
           <Form.Group className='mb-4 fatter-text centered-label'>
             <FloatingLabel label="New Password">
@@ -31,7 +72,7 @@ function ChangePassword() {
           </Form.Group>
 
           <Container className='text-center'>
-            <Button className='button-large btn-lg' variant='outline-success' >Change Password</Button>
+            <Button type="submit" className='button-large btn-lg' variant='outline-success' >Change Password</Button>
           </Container>
         </Form>
       </Card.Body>
@@ -39,5 +80,6 @@ function ChangePassword() {
   </Container>
   )
 }
+
 
 export default ChangePassword
