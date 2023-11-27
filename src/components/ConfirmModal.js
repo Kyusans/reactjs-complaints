3c82c0c91f0b935945029c481a1c1208e9c74314
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-11-27 11:13:34 +0800

    added Button with spinner in ConfirmModal.js

--- src/components/ConfirmModal.js
+++ src/components/ConfirmModal.js
@@ -1,66 +1,82 @@
 import axios from 'axios';
-import React, { useState } from 'react'
-import { Button, Modal } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Button, Modal, Spinner } from 'react-bootstrap';
 import AlertScript from './AlertScript';
 
 export default function ConfirmModal(props) {
-  const {show, hide, compId} = props;
+  const { show, hide, compId } = props;
   const [isJobComplete, setIsJobComplete] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  	//for alert
-	const [showAlert, setShowAlert] = useState(false);
-	const [alertVariant, setAlertVariant] = useState("");
-	const [alertMessage, setAlertMessage] = useState("");
+  // for alert
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertVariant, setAlertVariant] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
 
+  function getAlert(variantAlert, messageAlert) {
+    setShowAlert(true);
+    setAlertVariant(variantAlert);
+    setAlertMessage(messageAlert);
+  }
 
-	function getAlert(variantAlert, messageAlert){
-		setShowAlert(true);
-		setAlertVariant(variantAlert);
-		setAlertMessage(messageAlert);
-	}
+  const jobDone = () => {
+    setIsLoading(true);
 
-  const jobDone = () =>{
     const url = localStorage.getItem("url") + "personnel.php";
-    const jsonData = {compId: compId};
+    const jsonData = { compId: compId };
     const formData = new FormData();
     formData.append("operation", "jobDone");
     formData.append("json", JSON.stringify(jsonData));
-    axios({url: url, data: formData, method: "post"})
-    .then((res) =>{
-      if(res.data === 1){
-        getAlert("success","Job Complete!");
-        setIsJobComplete(true);
-        setTimeout(() => {
-          window.location.reload();
-        }, 1500);
-      }
-    })
-    .catch((err) =>{
-      alert("There was an unexpected error: " + err);
-    })
-  }
 
-  const handleHide = () =>{
+    axios({ url: url, data: formData, method: "post" })
+      .then((res) => {
+        if (res.data === 1) {
+          getAlert("success", "Job Complete!");
+          setIsJobComplete(true);
+          setTimeout(() => {
+            window.location.reload();
+          }, 1500);
+        }
+      })
+      .catch((err) => {
+        getAlert("danger", "There was an unexpected error: " + err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const handleHide = () => {
     setIsJobComplete(false);
     hide();
-  }
+  };
 
   return (
     <>
       <Modal show={show} onHide={handleHide} centered backdrop="static">
-        <Modal.Header><h4>Confirmation</h4></Modal.Header>
+        <Modal.Header>
+          <h4>Confirmation</h4>
+        </Modal.Header>
         <Modal.Body>
-          {!isJobComplete ? 
+          {!isJobComplete ? (
             "Are you sure you want to mark this job as completed?"
-          :
+          ) : (
             <AlertScript show={showAlert} variant={alertVariant} message={alertMessage} />
-          }
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button variant='outline-danger' onClick={handleHide}>Close</Button>
-          <Button variant='outline-success' onClick={jobDone}>Mark as done</Button>
+          {!isJobComplete && (
+            <Button variant='outline-success' onClick={jobDone} disabled={isLoading}>
+              {isLoading ? (
+                <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
+              ) : (
+                "Mark as done"
+              )}
+            </Button>
+          )}
         </Modal.Footer>
       </Modal>
     </>
-  )
+  );
 }
