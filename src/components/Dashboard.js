f2b34ab9e8c4cad6a74b67c6430150b5d4c04c1c
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-12-02 10:46:29 +0800

    removed table dashboard for personnel and clients and updated into card

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import React, { useCallback, useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock, faPlay, faCheck, faPlus, faSync } from '@fortawesome/free-solid-svg-icons';
-import { Button, Container, Spinner, Table } from 'react-bootstrap'
+import { faPlus, faSync } from '@fortawesome/free-solid-svg-icons';
+import { Button, Container, Spinner } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
 import ComplaintForm from './ComplaintForm';
 import "./css/site.css";
 import JobDetails, { formatDate } from './JobDetails';
 import UpdateTicketModal from './UpdateTicketModal';
+import TicketCard from './TicketCard';
 
 function Dashboard() {
   const navigateTo = useNavigate();
@@ -38,7 +39,7 @@ function Dashboard() {
       formData.append("json", JSON.stringify(jsonData));
 
       const response = await axios.post(url, formData);
-
+      console.log("response ni getComplaiknts: " +  JSON.stringify(response));
       if (response.data !== 0) {
         setTickets(response.data);
       }
@@ -57,7 +58,16 @@ function Dashboard() {
     } else {
       setCompId(id);
       setShowJobDetails(true);
-      // navigateTo(`/job/details/${id}`);
+    }
+  }
+
+  function formatStatus(status) {
+    if (status === 1) {
+      return "Pending"
+    }else if(status ===2){
+      return "On-Going"
+    }else{
+      return "Completed"
     }
   }
 
@@ -76,58 +86,25 @@ function Dashboard() {
       {localStorage.getItem("isLoggedIn") === "1" ? (
         <Container className="mt-3">
           <Container className='mb-2 mt-2 d-flex align-content-sm-start justify-content-start'>
-            <Button className='btn btn-success me-1' onClick={openComplaintModal}><FontAwesomeIcon icon={faPlus} /> Add Ticket</Button>
-            <Button onClick={getComplaints}><FontAwesomeIcon icon={faSync} /> Refresh</Button>
+            <Button className='btn btn-success me-1' onClick={openComplaintModal}>
+              <FontAwesomeIcon icon={faPlus} /> Add Ticket
+            </Button>
+            <Button onClick={getComplaints}>
+              <FontAwesomeIcon icon={faSync} /> Refresh
+            </Button>
           </Container>
-
+  
           {isLoading ? (
             <Container className='text-center'>
               <Spinner animation='border' variant='success' />
             </Container>
           ) : (
             <>
-              <Table striped bordered hover responsive variant='success' className='border-1'>
-                <thead>
-                  <tr>
-                    <th className="green-header">Subject</th>
-                    <th className="green-header">Status</th>
-                    <th className="green-header">Date</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {tickets.length === 0 ? (
-                    <tr>
-                      <td colSpan="3" className="text-center">No tickets available</td>
-                    </tr>
-                  ) : (
-                    tickets.map((ticket, index) => (
-                      <tr key={index} className='ticket-cell' onClick={() => handleNavigate(ticket.comp_id, ticket.comp_status)}>
-                        <td>{ticket.comp_subject}</td>
-                        <td>
-                          {ticket.comp_status === 1 ? (
-                            <span>
-                              <FontAwesomeIcon icon={faClock} className="me-2 pending" />
-                              Pending
-                            </span>
-                          ) : ticket.comp_status === 2 ? (
-                            <span>
-                              <FontAwesomeIcon icon={faPlay} className="me-2 ongoing" />
-                              On-Going
-                            </span>
-                          ) : (
-                            <span>
-                              <FontAwesomeIcon icon={faCheck} className="me-2 completed" />
-                              Completed
-                            </span>
-                          )}
-                        </td>
-                        <td className='ticket-date'>{formatDate(ticket.comp_date)}</td>
-                      </tr>
-                    ))
-                  )}
-                </tbody>
-              </Table>
-
+              {tickets.map((ticket, index) => (
+                <div key={index} className='ticket-cell' onClick={() => handleNavigate(ticket.comp_id, ticket.comp_status)}>
+                  <TicketCard subject={ticket.comp_subject} status={formatStatus(ticket.comp_status)} priority={null} date={formatDate(ticket.comp_date)} />
+                </div>
+              ))}
             </>
           )}
         </Container>
@@ -139,6 +116,6 @@ function Dashboard() {
       <JobDetails show={showJobDetails} onHide={hideJobDetails} compId={compId} />
     </>
   );
-
+  
 }
 export default Dashboard;
--- src/components/TicketCard.js
+++ src/components/TicketCard.js
@@ -1,4 +1,4 @@
-import { faArrowDown, faArrowRight, faArrowUp, faCheck, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDown, faArrowRight, faArrowUp, faCheck, faClock, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import { Card, Col, Container, Row } from 'react-bootstrap'
@@ -10,28 +10,33 @@ function TicketCard(props) {
       <Card
         border='dark'
         className="text-light"
-        bg={status === "On-Going" ? "secondary" : "success"}
+        bg={status === "On-Going" ? "secondary" : status === "Completed" ? "success" : "dark"}
       >
         <Container className='p-3'>
           <Row>
             <Col xs={12} md={3}><b>Subject:</b> {subject}</Col>
-            <Col xs={12} md={3}><b className='me-2'>Priority:</b>
-              {
-                priority === "High" ?
-                  <FontAwesomeIcon icon={faArrowUp} className="text-danger me-2" />
-                  :
-                  priority === "Medium" ?
-                    <FontAwesomeIcon icon={faArrowRight} className="text-warning me-2" />
+            {priority === null ? <></> :
+              <Col xs={12} md={3}><b className='me-2'>Priority:</b>
+                {
+                  priority === "High" ?
+                    <FontAwesomeIcon icon={faArrowUp} className="text-danger me-2" />
                     :
-                    <FontAwesomeIcon icon={faArrowDown} className="text-light me-2" />
-              }
-            </Col>
+                    priority === "Medium" ?
+                      <FontAwesomeIcon icon={faArrowRight} className="text-warning me-2" />
+                      :
+                      <FontAwesomeIcon icon={faArrowDown} className="text-light me-2" />
+                }
+              </Col>
+            }
             <Col xs={12} md={3}><b className='me-2'>Status:</b>
               {
                 status === "Completed" ?
                   <FontAwesomeIcon icon={faCheck} />
                   :
-                  <FontAwesomeIcon icon={faPlay} className='text-warning' />
+                  status === "Pending" ?
+                    <FontAwesomeIcon icon={faClock} />
+                    :
+                    <FontAwesomeIcon icon={faPlay} className='text-warning' />
               }
             </Col>
             <Col xs={12} md={3}><b>Date:</b> {date}</Col>
