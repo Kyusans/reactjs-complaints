a5c101a0a3fafd33e3bb732e323832b442d73ae2
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-12-27 02:09:05 +0800

    added startdate only switch functions for personnel's calendar view

--- src/components/PersonnelJobCalendarView.js
+++ src/components/PersonnelJobCalendarView.js
@@ -3,14 +3,17 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import React, { useCallback, useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
+import { Button, Container, Spinner } from 'react-bootstrap'
 import axios from 'axios';
 import JobDetails from './JobDetails'
 
 function PersonnelJobCalendarView() {
+  const [originalEvents, setOriginalEvents] = useState([]);
   const [events, setEvents] = useState([]);
   const [ticketId, setTicketId] = useState("");
   const [showJobDetails, setShowJobDetails] = useState(false);
+  const [startDateOnly, setStartDateOnly] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const hideJobDetails = () => {
     getJobTicket();
@@ -18,6 +21,7 @@ function PersonnelJobCalendarView() {
   }
 
   const getJobTicket = useCallback(async () => {
+    setIsLoading(true);
     try {
       const url = localStorage.getItem("url") + "personnel.php";
       const userId = localStorage.getItem("userId");
@@ -27,6 +31,8 @@ function PersonnelJobCalendarView() {
       formData.append("json", JSON.stringify(jsonData));
       const res = await axios({ url: url, data: formData, method: "post" });
       if (res.data !== 0) {
+        console.log("Res.data ni getJobTicket", JSON.stringify(res.data));
+        setOriginalEvents(res.data);
         const formattedEvents = res.data.map((job) => ({
           id: job.job_complaintId,
           title: job.job_title,
@@ -38,8 +44,26 @@ function PersonnelJobCalendarView() {
       }
     } catch (error) {
       alert("There was an unexpected error: " + error);
+    } finally {
+      // setIsLoading(false);
     }
   }, []);
+
+  const handleStartDateOnly = (status) => {
+    setStartDateOnly(status === 1);
+    updateEvent();
+  }
+
+  function updateEvent() {
+    const updateEvent = originalEvents.map((job) => ({
+      id: job.job_complaintId,
+      title: job.job_title,
+      start: new Date(job.job_createDate),
+      end: startDateOnly ? null : new Date(job.comp_end_date),
+      color: colorFormatter(job.joStatus_name),
+    }));
+    setEvents(updateEvent);
+  }
 
   function handleEventClick(info) {
     setTicketId(info.event.id);
@@ -53,22 +77,36 @@ function PersonnelJobCalendarView() {
   const maxEventsToShow = 4;
 
   return (
-    <Container fluid className="vh-100 text-white scrollable-container">
-      <FullCalendar
-        className="clickable"
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView='dayGridMonth'
-        weekends={true}
-        events={events || []}
-        dayMaxEvents={events && events.length >= maxEventsToShow ? maxEventsToShow : false}
-        eventClick={handleEventClick}
-        headerToolbar={{
-          left: 'title',
-          center: '',
-          right: 'today,prev,next dayGridMonth,timeGridWeek,timeGridDay'
-        }}
-        height={'90vh'}
-      />
+    <Container fluid className="mt-2 vh-100 text-white scrollable-container">
+      <Container>
+        {!startDateOnly ?
+          (<Button onClick={() => handleStartDateOnly(1)} className='ms-2'>Show Start Date Only</Button>)
+          :
+          (<Button onClick={() => handleStartDateOnly(0)} className='btn-secondary ms-2'>Include Deadline</Button>)
+        }
+
+        {isLoading ?
+          <Container className='text-center mt-3'>
+            <Spinner animation='border' variant='success' />
+          </Container>
+          :
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            initialView='dayGridMonth'
+            weekends={true}
+            events={events || []}
+            dayMaxEvents={events && events.length >= maxEventsToShow ? maxEventsToShow : false}
+            eventClick={handleEventClick}
+            headerToolbar={{
+              left: 'title',
+              center: '',
+              right: 'today,prev,next dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            height={'90vh'}
+          />
+        }
+
+      </Container>
       <JobDetails show={showJobDetails} onHide={hideJobDetails} compId={ticketId} />
     </Container>
   )
