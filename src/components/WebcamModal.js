2c63d567976c80527e49762cd7643ccbca56096d
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-12-30 12:43:32 +0800

    added spinner in webcam modal

--- src/components/WebcamModal.js
+++ src/components/WebcamModal.js
@@ -1,20 +1,35 @@
 import React, { useCallback, useRef, useState } from 'react';
-import { Button, Container, FloatingLabel, Form, Modal, Row } from 'react-bootstrap';
+import { Button, Container, FloatingLabel, Form, Modal, Row, Spinner } from 'react-bootstrap';
 import Webcam from 'react-webcam';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
+import AlertScript from './AlertScript';
 
 function WebcamModal(props) {
   const { show, onHide, compId } = props;
   const [commentText, setCommentText] = useState("");
   const webcamRef = useRef(null);
   const [capturedImage, setCapturedImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [videoConstraints, setVideoConstraints] = useState({
     facingMode: 'user',
   });
 
+  //for alert
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertVariant, setAlertVariant] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+
+
+  function getAlert(variantAlert, messageAlert) {
+    setShowAlert(true);
+    setAlertVariant(variantAlert);
+    setAlertMessage(messageAlert);
+  }
+
   const handleCaptureSubmit = async () => {
+    setIsLoading(true);
     try {
       const url = localStorage.getItem("url") + "users.php";
       const userId = localStorage.getItem("facultyLoggedIn")
@@ -43,25 +58,28 @@ function WebcamModal(props) {
       });
       switch (res.data) {
         case 1:
+          getAlert("success", "Success!");
+          setTimeout(() => {
+            handleOnHide();
+          }, 1000);
           break;
         case 2:
-          alert("You cannot Upload files of this type!");
+          getAlert("danger", "You cannot Upload files of this type!");
           break;
         case 3:
-          alert("There was an error uploading your file!");
+          getAlert("danger", "There was an error uploading your file!");
           break;
         case 4:
-          alert("Your file is too big (25mb maximum)");
+          getAlert("danger", "Your file is too big (25mb maximum)");
           break;
         default:
-          alert("Unsuccessful");
+          getAlert("danger", "Unsuccessful");
           break;
       }
     } catch (error) {
-      // Handle errors
       console.error(error);
     } finally {
-      handleOnHide();
+      setIsLoading(false);
     }
   };
 
@@ -82,6 +100,7 @@ function WebcamModal(props) {
   };
 
   const handleOnHide = () => {
+    setShowAlert(false);
     setCommentText("");
     setCapturedImage(null);
     onHide();
@@ -92,10 +111,11 @@ function WebcamModal(props) {
       <Modal show={show} onHide={onHide} size='lg' className='bg-dark' backdrop="static" centered>
         <Form>
           <Modal.Body>
-            <Button variant='outline-danger button-m' onClick={onHide}>
+            <Button variant='outline-danger button-m' onClick={handleOnHide}>
               <FontAwesomeIcon icon={faArrowLeft} />
             </Button>
-            <hr/>
+            <hr />
+            <AlertScript show={showAlert} variant={alertVariant} message={alertMessage} />
             <Container className='text-center mt-3'>
               {capturedImage ? (
                 <img src={capturedImage} alt="Captured" className="img-fluid" />
@@ -113,21 +133,29 @@ function WebcamModal(props) {
               <Row className='d-flex justify-content-center mt-3'>
                 {capturedImage ? (
                   <>
-                    <Container className='mt-3 mb-3'>
-                      <FloatingLabel label="Add comment">
-                        <Form.Control type='text' value={commentText}
-                          onChange={(e) => setCommentText(e.target.value)}
-                          placeholder='Add comment'
-                        />
-                      </FloatingLabel>
-                    </Container>
+                    {isLoading ?
+                      <Container className='text-center mt-2'>
+                        <Spinner variant='success' />
+                      </Container>
+                      :
+                      <>
+                        <Container className='mt-3 mb-3'>
+                          <FloatingLabel label="Add comment">
+                            <Form.Control type='text' value={commentText}
+                              onChange={(e) => setCommentText(e.target.value)}
+                              placeholder='Add comment'
+                            />
+                          </FloatingLabel>
+                        </Container>
+                        <Button variant='outline-success big-height w-50 me-1' onClick={handleCaptureSubmit}>
+                          Submit
+                        </Button>
+                        <Button variant='outline-danger big-height w-50 mt-2' onClick={handleRetake}>
+                          Retake
+                        </Button>
+                      </>
+                    }
 
-                    <Button variant='outline-success big-height w-50 me-1' onClick={handleCaptureSubmit}>
-                      Submit
-                    </Button>
-                    <Button variant='outline-danger big-height w-50 mt-2' onClick={handleRetake}>
-                      Retake
-                    </Button>
 
                   </>
                 ) : (
