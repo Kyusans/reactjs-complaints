c9a0edfc3ac3698ca4717d047f87c8a7214cbce7
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2024-01-16 10:03:25 +0800

    Updated ConfirmModal.js

--- src/components/ConfirmModal.js
+++ src/components/ConfirmModal.js
@@ -1,20 +1,60 @@
 import axios from 'axios';
-import React, { useState } from 'react';
-import { Button, Container, FloatingLabel, Form, Modal, Spinner } from 'react-bootstrap';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Container, FloatingLabel, Form, ListGroup, Modal, Spinner } from 'react-bootstrap';
 import AlertScript from './AlertScript';
 
 export default function ConfirmModal(props) {
   const { show, hide, compId } = props;
   const [isLoading, setIsLoading] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isOther, setIsOther] = useState(false);
   const [selectedOperation, setSelectedOperation] = useState("");
   const [remarks, setRemarks] = useState("");
   const [item, setItem] = useState("");
+  const [otherEquipment, setOtherEquipment] = useState("");
   const [validated, setValidated] = useState(false);
+  const [equipments, setEquipments] = useState([]);
+  const [selectedEquipment, setSelectedEquipment] = useState([]);
+
   const operationData = [
     { "operation_name": "Replaced" },
     { "operation_name": "Repaired" },
     { "operation_name": "Destroyed" },
   ];
+
+  const handleHide = () => {
+    setIsSubmitted(false);
+    setOtherEquipment("");
+    setEquipments([]);
+    setIsOther(false);
+    setShowAlert(false);
+    setIsLoading(false);
+    setRemarks("");
+    setSelectedOperation("");
+    setItem("");
+    setValidated(false);
+    setSelectedEquipment([]);
+    hide();
+  };
+
+
+  const handleEquipmentChange = (selectedEquipmentId) => {
+    setItem(selectedEquipmentId);
+    setIsOther(selectedEquipmentId === 'other');
+    handleAddEquipment();
+  };
+
+  const handleAddEquipment = useCallback(() => {
+    if (item && !selectedEquipment.includes(item) && !isOther) {
+      setSelectedEquipment([...selectedEquipment, item]);
+      // setItem('');
+    }
+  }, [isOther, item, selectedEquipment]);
+
+  const handleRemoveEquipment = (removedEquipmentId) => {
+    const updatedEquipmentArray = selectedEquipment.filter(id => id !== removedEquipmentId);
+    setSelectedEquipment(updatedEquipmentArray);
+  };
 
   // for alert
   const [showAlert, setShowAlert] = useState(false);
@@ -28,14 +68,18 @@ export default function ConfirmModal(props) {
   }
 
   const jobDone = async () => {
-    setIsLoading(true);
+    setIsSubmitted(true);
     try {
       const url = localStorage.getItem("url") + "personnel.php";
       const fullName = localStorage.getItem("userFullName");
-      const jsonData = { compId: compId, fullName: fullName, item: item, jobOperation: selectedOperation, remarks: remarks };
+      const master = { compId: compId, fullName: fullName, jobOperation: selectedOperation, remarks: remarks };
+      const detail = { selectedEquipment: selectedEquipment };
+      const jsonData = {master: master, detail: detail};
       const formData = new FormData();
       formData.append("operation", "jobDone");
       formData.append("json", JSON.stringify(jsonData));
+
+      console.log("json data ko to", JSON.stringify(jsonData));
 
       const response = await axios.post(url, formData);
 
@@ -53,10 +97,41 @@ export default function ConfirmModal(props) {
     } catch (err) {
       getAlert("danger", "There was an unexpected error: " + err);
     } finally {
-      setIsLoading(false);
-
+      setIsSubmitted(false);
     }
   };
+
+  const getEquipment = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const url = localStorage.getItem("url") + "admin.php";
+      const formData = new FormData();
+      formData.append("operation", "getEquipment");
+
+      const res = await axios.post(url, formData);
+      console.log("res ni getEquipment", JSON.stringify(res.data));
+
+      if (res.data !== 0) {
+        setEquipments(res.data);
+      } else {
+        getAlert("danger", "No Equipment yet");
+      }
+    } catch (error) {
+      getAlert("danger", "There was an error occurred: " + error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (show) {
+      getEquipment();
+    }
+  }, [getEquipment, show]);
+
+  useEffect(() => {
+    handleAddEquipment();
+  }, [handleAddEquipment])
 
   const handleJobDone = (e) => {
     const form = e.currentTarget;
@@ -69,60 +144,77 @@ export default function ConfirmModal(props) {
     setValidated(true);
   }
 
-  const handleHide = () => {
-    setShowAlert(false);
-    setIsLoading(false);
-    setRemarks("");
-    setSelectedOperation("");
-    setItem("");
-    setValidated(false);
-    hide();
-  };
 
   return (
     <>
-      <Modal show={show} className='bg-dark' onHide={handleHide} centered backdrop="static">
-        <Form noValidate validated={validated} onSubmit={handleJobDone}>
-          <Modal.Header>
-            <h4>Confirmation</h4>
-          </Modal.Header>
-          <Modal.Body>
-            <AlertScript show={showAlert} variant={alertVariant} message={alertMessage} />
-            <Container>
-              <FloatingLabel label="Operation">
-                <Form.Select className='mb-3' value={selectedOperation} onChange={(e) => setSelectedOperation(e.target.value)} placeholder='Operation' required>
-                  <option value={""}>Select operation</option>
-                  {operationData.map((operation, index) => (
-                    <option key={index} value={operation.operation_name}>{operation.operation_name}</option>
+      {isLoading ?
+        <Container className='text-center'>
+          <Spinner variant='success' animation='border' />
+        </Container>
+        :
+        <Modal show={show} className='bg-dark' onHide={handleHide} centered backdrop="static">
+          <Form noValidate validated={validated} onSubmit={handleJobDone}>
+            <Modal.Header>
+              <h4>Confirmation</h4>
+            </Modal.Header>
+            <Modal.Body>
+              <AlertScript show={showAlert} variant={alertVariant} message={alertMessage} />
+              <Container>
+                <FloatingLabel label="Operation">
+                  <Form.Select className='mb-3' value={selectedOperation} onChange={(e) => setSelectedOperation(e.target.value)} placeholder='Operation' required>
+                    <option value={""}>Select operation</option>
+                    {operationData.map((operation, index) => (
+                      <option key={index} value={operation.operation_name}>{operation.operation_name}</option>
+                    ))}
+                  </Form.Select>
+                </FloatingLabel>
+
+                <FloatingLabel label='Equipment'>
+                  <Form.Select
+                    placeholder='Item'
+                    value={item}
+                    onChange={(e) => handleEquipmentChange(e.target.value)}
+                    required
+                  >
+                    <option value={""}>Select equipment</option>
+                    {equipments.map((equipment, index) => (
+                      <option key={index} value={equipment.equip_name}>{equipment.equip_name}</option>
+                    ))}
+                    <option value="other">Other..</option>
+                  </Form.Select>
+                </FloatingLabel>
+
+                {isOther &&
+                  <FloatingLabel label='Other equipment name' className='mt-3'>
+                    <Form.Control placeholder='Other equipment name' value={otherEquipment} onChange={(e) => setOtherEquipment(e.target.value)} required />
+                  </FloatingLabel>
+                }
+
+                <FloatingLabel className='mt-3' label="Remarks">
+                  <Form.Control placeholder='Remarks' value={remarks} onChange={(e) => setRemarks(e.target.value)} required />
+                </FloatingLabel>
+
+                <ListGroup className='mt-3'>
+                  {selectedEquipment.map((selectedEquipment, index) => (
+                    <ListGroup.Item key={index} className='d-flex justify-content-between align-items-center'>
+                      {index + 1}. {selectedEquipment === "other" ? otherEquipment : selectedEquipment}
+                      <Button variant='outline-danger' size='sm' onClick={() => handleRemoveEquipment(selectedEquipment)}>Remove</Button>
+                    </ListGroup.Item>
                   ))}
-                </Form.Select>
-              </FloatingLabel>
+                </ListGroup>
+              </Container>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant='outline-secondary' onClick={handleHide}>Close</Button>
 
-              <FloatingLabel label='Item'>
-                <Form.Control placeholder='Item' value={item} onChange={(e) => setItem(e.target.value)} required />
-              </FloatingLabel>
-              <Form.Text className="text-muted ms-2">
-                For multiple items, use commas (e.g., 'chair, table').
-              </Form.Text>
-
-              <FloatingLabel className='mt-3' label="Remarks">
-                <Form.Control placeholder='Remarks' value={remarks} onChange={(e) => setRemarks(e.target.value)} required />
-              </FloatingLabel>
-            </Container>
-
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='outline-danger' onClick={handleHide}>Close</Button>
-
-            <Button type='submit' variant='outline-success' disabled={isLoading}>
-              {isLoading &&
-                <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />}
-              Mark as done
-            </Button>
-          </Modal.Footer>
-
-        </Form>
-      </Modal>
+              <Button type='submit' variant='outline-success' disabled={isLoading}>
+                {isSubmitted && <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />}
+                Mark as done
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal>
+      }
     </>
   );
 }
